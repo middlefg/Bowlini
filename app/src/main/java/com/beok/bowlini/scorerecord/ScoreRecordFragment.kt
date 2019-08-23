ca926c231c315264f0644038ac6b75a4038f025d
BeokBeok <kekemusa37@nate.com>
2019-08-23 23:32:50 +0900

    BaseFragment 생성 및 Fragment 추가

---
+++ app/src/main/java/com/beok/bowlini/scorerecord/ScoreRecordFragment.kt
@@ -0,0 +1,9 @@
+package com.beok.bowlini.scorerecord
+
+
+import com.beok.bowlini.R
+import com.beok.bowlini.base.BaseFragment
+
+class ScoreRecordFragment : BaseFragment(
+    R.layout.fragment_score_record
+)
