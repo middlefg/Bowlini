46aef7f6741508bd2121477352f62e8a2c4d5ab2
BeokBeok <kekemusa37@nate.com>
2019-10-28 20:54:11 +0900

    app 모듈 정리
    - MainActivity 를 제외한 모든 파일 제거

--- app/src/main/java/com/beok/bowlini/ocr/OcrActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.beok.bowlini.ocr
-
-import com.beok.bowlini.R
-import com.beok.bowlini.base.BaseActivity
-
-class OcrActivity : BaseActivity(
-    R.layout.activity_ocr
-)
--- app/src/main/java/com/beok/bowlini/ocr/OcrFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.beok.bowlini.ocr
-
-
-import com.beok.bowlini.R
-import com.beok.bowlini.base.BaseFragment
-
-class OcrFragment : BaseFragment(
-    R.layout.fragment_ocr
-)
