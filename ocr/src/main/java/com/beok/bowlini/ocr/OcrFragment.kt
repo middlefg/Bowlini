86f8998e52f2eec445b98f3af3bef9b4eb2cb80d
BeokBeok <kekemusa37@nate.com>
2019-10-30 21:56:17 +0900

    ocr 모듈 추가

---
+++ ocr/src/main/java/com/beok/bowlini/ocr/OcrFragment.kt
@@ -0,0 +1,22 @@
+package com.beok.bowlini.ocr
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OcrFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ocr, container, false)
+    }
+
+}
