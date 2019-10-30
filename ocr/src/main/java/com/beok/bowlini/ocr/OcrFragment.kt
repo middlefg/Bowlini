d42e7b228a42804a3e5fcd0ef01bcb1452ee32d5
BeokBeok <kekemusa37@nate.com>
2019-10-30 22:46:18 +0900

    ocrFragment 연결

--- ocr/src/main/java/com/beok/bowlini/ocr/OcrFragment.kt
+++ ocr/src/main/java/com/beok/bowlini/ocr/OcrFragment.kt
@@ -1,22 +1,9 @@
 package com.beok.bowlini.ocr
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.beok.bowlini.common.base.BaseFragment
+import com.beok.bowlini.common.base.BaseViewModel
+import com.beok.bowlini.ocr.databinding.FragmentOcrBinding
 
-/**
- * A simple [Fragment] subclass.
- */
-class OcrFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ocr, container, false)
-    }
-
-}
+class OcrFragment : BaseFragment<FragmentOcrBinding, BaseViewModel>(
+    R.layout.fragment_ocr
+)
